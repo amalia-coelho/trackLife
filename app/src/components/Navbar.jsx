09ba56c5fb0dc61e0e787e5fa1cad2a7def70e49
Amália Coelho <apenas.coelho03@gmail.com>
2024-08-22 23:20:37 -0300

    Importação da biblitoeca font awesome

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInbox } from '@fortawesome/free-solid-svg-icons'
 import '../styles/Navbar.css';
 
 function Navbar() {
@@ -6,21 +8,30 @@ function Navbar() {
         <>
             <header>
                 <div className="navbar-geral">
-                    <div className="navbar-logo">
-                        <span className="project-logo"> X </span> 
-                        <span className="project-name">TRACKLIFE</span>
+                    <div className="navbar-top">
+                        <div className="navbar-logo">
+                            <span className="project-logo"> X </span> 
+                            <span className="project-name">TRACKLIFE</span>
+                        </div>
+                        <div className="navbar-item">
+                            <FontAwesomeIcon icon="fa-solid fa-calendar-week" color="white"/>
+                            <span className="navbar-title">Today</span>
+                        </div>
+                        <div className="navbar-item">
+                            <FontAwesomeIcon icon={faInbox} color="white"/>    
+                            <span className="navbar-title">Inbox</span>
+                        </div>
                     </div>
-                    <div className="navbar-item">
-                        <span className="navbar-icon">X</span>
-                        <span className="navbar-title">Inbox</span>
+                    <div className="navbar-navigation">
+                        <div className="navbar-item">
+                            <FontAwesomeIcon icon="fa-solid fa-house" color="white"/>
+                            <span className="navbar-title">Tarefas</span>
+                        </div>
+                        <div className="navbar-item">
+                            <FontAwesomeIcon icon="fa-solid fa-inbox" color="white"/>    
+                            <span className="navbar-title">Hábitos</span>
+                        </div>
                     </div>
-                    <div className="navbar-item">
-                        <span className="navbar-icon">X</span>
-                        <span className="navbar-title">Inbox</span>
-                    </div>
-                </div>
-                <div className="navbar-navigation">
-
                 </div>
                 <div className="navbar-config">
 
