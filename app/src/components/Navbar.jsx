81fba358d61d023ca50c643951228f58cdedb526
Amália Coelho <apenas.coelho03@gmail.com>
2024-08-22 22:09:28 -0300

    Primeiros passos

---
+++ app/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import '../styles/Navbar.css';
+
+function Navbar() {
+
+    return (
+        <>
+            <header>
+                <div className="navbar-geral">
+                    <div className="navbar-logo">
+                        <span className="project-logo"> X </span> 
+                        <span className="project-name">TRACKLIFE</span>
+                    </div>
+                    <div className="navbar-item">
+                        <span className="navbar-icon">X</span>
+                        <span className="navbar-title">Inbox</span>
+                    </div>
+                    <div className="navbar-item">
+                        <span className="navbar-icon">X</span>
+                        <span className="navbar-title">Inbox</span>
+                    </div>
+                </div>
+                <div className="navbar-navigation">
+
+                </div>
+                <div className="navbar-config">
+
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Navbar
